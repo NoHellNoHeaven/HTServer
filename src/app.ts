c7a921a88bc98fd768df2ae7efc70dfbc9f6041e
Gatoxic0 <re.romanini@duocuc.cl>
2025-07-17 23:38:06 -0400

    metodo para eliminar choferes

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: true,
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
